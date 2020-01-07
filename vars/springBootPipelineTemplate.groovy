222e0a2225e8dc9dc4525afc33b2a916ebbe4377
mmicciolo <mmicciolo@wpi.edu>
2020-01-07 09:57:00 -0500

    Update springBootPipelineTemplate.groovy

--- vars/springBootPipelineTemplate.groovy
+++ vars/springBootPipelineTemplate.groovy
@@ -3,11 +3,6 @@ def call(Map pipelineParams) {
     pipeline {
         agent any
         stages {
-            stage('git clone') {
-                steps {
-                    git(url: pipelineParams.gitUrl, credentialsId: pipelineParams.gitCredentials, branch: pipelineParams.gitBranch)
-                }
-            }
             stage('build') {
                 steps {
                     sh 'mvn -DskipTests package'
